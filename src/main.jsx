b0b0c1c447e0096d58751d184deae9dc4c886d4b
capibarbara <157847107+capibarbara@users.noreply.github.com>
2026-06-23 13:09:36 -0700

    Add admin route and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import Admin from './Admin'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(<App />)
+function Router(){
+  const path = window.location.pathname
+  if (path === '/admin') return <Admin />
+  return <App />
+}
+
+createRoot(document.getElementById('root')).render(<Router />)
